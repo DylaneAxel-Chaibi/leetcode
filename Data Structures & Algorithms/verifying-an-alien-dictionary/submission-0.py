d8db0dfb81b82ba2b4a9c8caf503db2a3fa7c15b
CHAIBI Dylane Axel <dylaneaxelc@gmail.com>
2026-04-29 21:15:36 +0100

    Add: verifying-an-alien-dictionary - submission-0

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        alphabet = {c : i for i, c in enumerate(order)}
+
+        for i in range(len(words) - 1) :
+            word1, word2 = words[i], words[i + 1]
+            for j in range(len(word1)) :
+                if j == len(word2) :
+                    return False
+                if word1[j] != word2[j] :
+                    if alphabet[word1[j]] > alphabet[word2[j]] :
+                        return False
+                    if alphabet[word1[j]] < alphabet[word2[j]] :
+                        break
+        return True
